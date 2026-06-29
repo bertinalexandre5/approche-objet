7c595c852394e780ff2561bf8aac9783f8f388dc
Alexandre Bertin <bertin.alexandre5@gmail.com>
2026-06-29 17:06:37 +0200

    TP 09 interfaces - ok

---
+++ src/main/java/fr/diginamic/interfaces/TestObjetGeometrique.java
@@ -0,0 +1,20 @@
+package fr.diginamic.interfaces;
+
+import java.util.ArrayList;
+
+public class TestObjetGeometrique {
+    public static void main(String[] args) {
+
+    ObjetGeometrique[] tab = new ObjetGeometrique[2];
+
+    tab[0] = new Cercle(5.0);
+    tab[1] = new Rectangle(15.0,10.5);
+
+    for (int i = 0; i < tab.length; i++) {
+        System.out.println("Périmètre : " + tab[i].perimetre());
+        System.out.println("Surface : " + tab[i].surface());
+
+    }
+
+    }
+}
